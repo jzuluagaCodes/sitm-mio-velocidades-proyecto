c0759cd7aa6f6d265e198bf7314de237e7271493
Johan Jojoa <johanjojoabucheli@gmail.com>
2026-06-10 22:01:55 -0500

    Mensaje paquetes recibidos

--- distribuido-ice/src/main/java/edu/icesi/sitmmio/ice/CalculadoraImpl.java
+++ distribuido-ice/src/main/java/edu/icesi/sitmmio/ice/CalculadoraImpl.java
@@ -22,6 +22,8 @@ public final class CalculadoraImpl implements sitmmio.Calculadora {
                            String[] activeRoutes,
                            Current current) {
 
+        System.out.println("\n[Worker] ¡Paquete recibido! Procesando " + data.length + " filas de datagramas...");
+
         List<Map<String, String>> rows = Arrays.asList(partition);
         Set<String> routeSet = new HashSet<>(Arrays.asList(activeRoutes));
 
